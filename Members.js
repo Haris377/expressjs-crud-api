1206f2ce7f66499f1e5c5b4e58bfd37f563348f0
Haris377 <Haris377@users.noreply.github.com>
2020-08-16 15:26:48 +0500

    initial commit

---
+++ Members.js
@@ -0,0 +1,28 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'John@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Williams',
+        email: 'Bob@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Shannon Jackson',
+        email: 'Shannon@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 4,
+        name: 'Epsom',
+        email: 'Epsom@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
